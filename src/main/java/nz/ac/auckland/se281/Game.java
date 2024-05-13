4d6330929f26126cc20ef371d8356a1c5975daa8
Henry Ly <lyhenry2004@gmail.com>
2024-05-13 14:13:37 +1200

    Play command now displays round number and gets input

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -1,16 +1,29 @@
 package nz.ac.auckland.se281;
 
+import javax.swing.plaf.metal.MetalProgressBarUI;
+
 import nz.ac.auckland.se281.Main.Choice;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  int roundNumber = 0;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
-    // the first element of options[0]; is the name of the player
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    roundNumber++;
   }
 
-  public void play() {}
+  public void play() {
+    String input = "";
+    MessageCli.START_ROUND.printMessage(String.valueOf(roundNumber));
+    MessageCli.ASK_INPUT.printMessage();
+    
+    while (!Utils.isInteger(input)) {
+      input = Utils.scanner.nextLine();
+    }
+  }
 
   public void endGame() {}
 
