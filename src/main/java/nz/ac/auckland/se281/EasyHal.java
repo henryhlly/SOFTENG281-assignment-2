80f78361e57036f8d109698ad1f22bb3ac61e7a1
Henry Ly <lyhenry2004@gmail.com>
2024-05-14 14:50:48 +1200

    Decided to go back to using StrategyMaster

--- src/main/java/nz/ac/auckland/se281/EasyHal.java
+++ src/main/java/nz/ac/auckland/se281/EasyHal.java
@@ -3,14 +3,11 @@ package nz.ac.auckland.se281;
 // Object for difficulty EASY
 public class EasyHal implements Hal9000 {
 
-  private Strategy strat;
-
-  public EasyHal() {
-    strat = new RandomStrat();
-  }
+  public EasyHal() {}
 
   @Override
   public int play() {
-    return strat.makeDecision();
+    StrategyMaster hal = new StrategyMaster(new RandomStrat());
+    return hal.think();
   }
 }
