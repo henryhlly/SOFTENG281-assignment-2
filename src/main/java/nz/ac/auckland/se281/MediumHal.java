28b1590600dc9819654d2052c908acafe8afba59
Henry Ly <lyhenry2004@gmail.com>
2024-05-14 14:01:34 +1200

    Implemented the top strategy, not tested

--- src/main/java/nz/ac/auckland/se281/MediumHal.java
+++ src/main/java/nz/ac/auckland/se281/MediumHal.java
@@ -1,7 +1,6 @@
 package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
-import nz.ac.auckland.se281.Main.Choice;
 
 // Object for difficulty MEDIUM
 public class MediumHal implements Hal9000 {
@@ -11,29 +10,6 @@ public class MediumHal implements Hal9000 {
 
   @Override
   public int play() {
-    int numberOfEven = 0;
-    int numberOfOdd = 0;
-    
-    // Calculate the number of even
-    for (int i : playerHistory) {
-      if (Utils.isEven(i)) {
-        numberOfEven++;
-      }
-    }
-    numberOfOdd = playerHistory.size() - numberOfEven;
-
-    if ((playerHistory.size() > 3) & (numberOfEven != numberOfOdd)) {
-      
-      if (numberOfEven > numberOfOdd) {
-        // When Hal thinks player will choose EVEN
-
-      } else {
-        // When Hal thinks player will choose ODD
-
-      }
-
-    } else {
-      return Utils.getRandomNumberRange(0, 5);
-    }
+    return -1;
   }
 }
--- src/main/java/nz/ac/auckland/se281/TopStrat.java
+++ src/main/java/nz/ac/auckland/se281/TopStrat.java
@@ -1,9 +1,53 @@
 package nz.ac.auckland.se281;
 
-public class RandomStrat implements Strategy {
-  public int makeDecision() {
-    return Utils.getRandomNumberRange(0, 5);
-  }
-}
+import nz.ac.auckland.se281.Main.Choice;
+import java.util.ArrayList;
 
-public class TopStrat implements Strategy 
+public class TopStrat implements Strategy {
+  
+  Choice choice;
+  int numberOfEven;
+  int numberOfOdd;
+
+  public TopStrat(Choice choice, ArrayList<Integer> playerHistory) {
+    this.choice = choice;
+
+    // Calculate the number of even and odd
+    for (int i : playerHistory) {
+      if (Utils.isEven(i)) {
+        numberOfEven++;
+      }
+    }
+    numberOfOdd = playerHistory.size() - numberOfEven;
+  }
+
+  public int makeDecision() {
+    switch (choice) {
+      case EVEN:
+        if (numberOfEven > numberOfOdd) {
+          // When Hal thinks player will choose EVEN
+          // EVEN + ODD = ODD
+          return Utils.getRandomOddNumber();
+
+        } else {
+          // When Hal thinks player will choose ODD
+          // ODD + EVEN = ODD
+          return Utils.getRandomEvenNumber();
+        }
+
+      case ODD:
+        if (numberOfEven > numberOfOdd) {
+          // When Hal thinks player will choose EVEN
+          // EVEN + EVEN = EVEN
+          return Utils.getRandomEvenNumber();
+
+        } else {
+          // When Hal thinks player will choose ODD
+          // ODD + ODD = EVEN
+          return Utils.getRandomOddNumber();
+        }
+    }
+    return -1;
+  }
+
+}
